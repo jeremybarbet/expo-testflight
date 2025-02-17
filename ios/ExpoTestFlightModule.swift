5699381291ebd6d4726b841911ef273be0076ce3
Jérémy Barbet <jeremgraph@gmail.com>
2025-02-17 09:41:56 +0000

    0.1.3

--- ios/ExpoTestFlightModule.swift
+++ ios/ExpoTestFlightModule.swift
@@ -3,9 +3,9 @@ import ExpoModulesCore
 public class ExpoTestFlightModule: Module {
   public func definition() -> ModuleDefinition {
     Name("ExpoTestFlight")
-    
+
     Function("isTestFlight") { () -> Bool in
-#if DEBUG
+#if DEBUG || targetEnvironment(simulator)
       return false
 #else
       return Bundle.main.appStoreReceiptURL?.lastPathComponent == "sandboxReceipt"
