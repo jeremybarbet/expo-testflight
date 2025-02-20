81adab937aa53272221674448d202d53bce02f97
Jérémy Barbet <jeremgraph@gmail.com>
2025-02-20 22:02:44 +0000

    refactor: add android module + use constants

--- ios/ExpoTestFlightModule.swift
+++ ios/ExpoTestFlightModule.swift
@@ -4,12 +4,8 @@ public class ExpoTestFlightModule: Module {
   public func definition() -> ModuleDefinition {
     Name("ExpoTestFlight")
 
-    Function("isTestFlight") { () -> Bool in
-#if DEBUG || targetEnvironment(simulator)
-      return false
-#else
-      return Bundle.main.appStoreReceiptURL?.lastPathComponent == "sandboxReceipt"
-#endif
-    }
+    Constants([
+      "isTestFlight": Bundle.main.appStoreReceiptURL?.lastPathComponent == "sandboxReceipt",
+    ])
   }
 }
--- src/ExpoTestFlightModule.ts
+++ src/ExpoTestFlightModule.ts
@@ -1,8 +1,7 @@
 import { NativeModule, requireNativeModule } from "expo";
 
 declare class ExpoTestFlightModule extends NativeModule {
-  isTestflight(): boolean;
+  isTestflight: boolean;
 }
 
-// This call loads the native module object from the JSI.
 export default requireNativeModule<ExpoTestFlightModule>("ExpoTestFlight");
